63baa4bb0a637cd44c1632df2404ac30cc49772e
Denisa Melusova <d.melusova@gmail.com>
2023-08-21 17:21:24 +0800

    creating booking model

---
+++ db/migrate/20230821091518_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.integer :user_id
+      t.integer :item_id
+      t.date :from
+      t.date :to
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_075904) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_091518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.date "from"
+    t.date "to"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
