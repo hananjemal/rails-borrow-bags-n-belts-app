31cd98cad0be9a5857ac649bcd5329a61a837054
hanan jemal <hanj50@gmail.com>
2023-08-21 17:14:54 +0800

    created items model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_081639) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_091002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "brand"
+    t.string "category"
+    t.string "color"
+    t.string "size"
+    t.string "picture_url"
+    t.string "description"
+    t.float "price"
+    t.boolean "availability"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
