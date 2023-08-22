09f8ce3d1eb2128dfa5f1e9c90d062b15c133c34
hanan jemal <hanj50@gmail.com>
2023-08-22 13:51:59 +0800

    done with routing for ne item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,22 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category, :brand, :size, :color, :description, :price, :availability, :photo)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Item.destroy_all
+
+items = Item.create(category: "belt", brand: "gucci", size: "M", color: "black", description: "90% brand new", price: 50, picture_url: "https://media.istockphoto.com/id/492845800/photo/black-belt.webp?b=1&s=170667a&w=0&k=20&c=zRgabxSREm66MUGU6iP41JZkJPrGWhrnRSbTEc4nOMo=", availability: true)
+items = Item.create(category: "bag", brand: "chanel", size: "S", color: "pink", description: "50% brand new", price: 50, picture_url: "https://images.unsplash.com/photo-1566150905458-1bf1fc113f0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmFnfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60", availability: true)
+
+puts "items created"
