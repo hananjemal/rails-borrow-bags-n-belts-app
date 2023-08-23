ef6a5a45c6ffe82fcdb7a45e0aa260424ed4261c
hanan jemal <hanj50@gmail.com>
2023-08-23 16:11:23 +0800

    done with booking back-end

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   has_one_attached :photo
+  has_many :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :items
-    # resources :bookings, only: [:new,:create]
-    get 'profile', to: 'users#show'
+  resources :items do
+    resources :bookings, only: [:new,:create]
+  end
+  get 'profile', to: 'users#show'
+  #  resources :bookings, only: [:new,:create]
 end
