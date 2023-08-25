d25eb83a1ec4c45ee69c3aa444ed9b01befb7f24
Denisa Melusova <d.melusova@gmail.com>
2023-08-25 13:36:53 +0800

    half way

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   BRANDS = ["Gucci", "Louis Vuitton", "Chanel", "Prada", "Hermès", "Coach", "Michael Kors"]
   COLORS = ["black", "white", "brown", "green", "red", "grey", "blue"]
   SIZE = ["small", "medium", "large"]
+  CATEGORY = ["Bag", "Belt"]
   has_one_attached :photo
   include PgSearch::Model
   pg_search_scope :search_by_full_text, against: [ :brand, :category, :color, :size, :price ], using: {
